8ffebb6c199b2182a44205848d7898fe41ec717a
RishabhAggarwal4198 <aggarwalrishabh4198@gmail.com>
2024-07-12 14:00:15 +0530

    Changes in the documentaion

--- Revising OLD ones/Practice_1.js
+++ Revising OLD ones/Practice_1.js
@@ -10,6 +10,13 @@ let gy =  "Texas"
 // console.log(gy.charAt(2)); 
 
 let gh = "King of Vidarbh "
-console.log(gh.charAt(4));
-console.log(gh.charAt(5));
-console.log(gh.charAt(8));
+// console.log(gh.charAt(4));
+// console.log(gh.charAt(5));
+// console.log(gh.charAt(8));
+
+
+let y = 20;
+let z = 30
+
+let bb = Math.floor(Math.random() * (11))
+
